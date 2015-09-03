88fcc5ed0d434c0700d855c6dbc5a8acfc0f55e2
Earl Forrest <aef_21@hotmail.com>
2015-09-03 04:49:52 +0000

    Changes made by Earl Forrest (aef_21@hotmail.com).  add avatar upload to sign up view

--- UZone Hybrid/scripts/app/signup.js
+++ UZone Hybrid/scripts/app/signup.js
@@ -15,6 +15,7 @@ app.Signup = (function () {
         //var $signupInfo;
         var $signupBtnWrp;
         var validator;
+        var userAvatarUri;
 
         // Register user after required fields (username and password) are validated in Backend Services
         var signup = function () {
@@ -27,7 +28,9 @@ app.Signup = (function () {
             //}
 
             //dataSource.BirthDate = birthDate;
-
+            console.log(dataSource);
+            dataSource.AvatarUri = userAvatarUri;
+            console.log(dataSource);
             Everlive.$.Users.register(
                     dataSource.Username,
                     dataSource.Password,
@@ -40,6 +43,25 @@ app.Signup = (function () {
                         app.showError(err.message);
                     });
         };
+
+        var addAvatar = function () {
+            var success = function (data) {
+                Everlive.$.Files.create({
+                    Filename: Math.random().toString(36).substring(2, 15) + ".jpg",
+                    ContentType: "image/jpeg",
+                    base64: data
+                }).then(loadPhotos);
+            };
+            var error = function () {
+                navigator.notification.alert("Unfortunately we could not add the image");
+            };
+            var config = {
+                destinationType: Camera.DestinationType.DATA_URL,
+                targetHeight: 400,
+                targetWidth: 400
+            };
+            navigator.camera.getPicture(success, error, config);
+        }
 
         // Executed after Signup view initialization
         // init form validator
@@ -60,8 +82,6 @@ app.Signup = (function () {
                     $signupBtnWrp.addClass('disabled');
                 }
             });
-
-
         };
 
         // Executed after show of the Signup view
@@ -75,7 +95,8 @@ app.Signup = (function () {
                 Email: '',
                 School: '0',
                 Major: '',
-                BirthDate: ''               
+                BirthDate: '',
+                AvatarUri: userAvatarUri
                     //About: '',
                     //Friends: [],
                     // BirthDate: new Date()
@@ -93,13 +114,27 @@ app.Signup = (function () {
             var selected = sel.options[sel.selectedIndex].value;
             sel.style.color = (selected === 0) ? '#b6c5c6' : '#34495e';
         }
-        
+
+        function loadPhotos() {
+            Everlive.$.Files.get().then(function (data) {
+                var files = [];
+                files.push(data.result[data.count - 1].Uri)
+                userAvatarUri = data.result[data.count - 1].Uri;
+                console.log(userAvatarUri);
+                $("#avatarImage").kendoMobileListView({
+                    dataSource: files,
+                    template: "<img src='#: data #'>"
+                });
+            });
+        }
+
         return {
             init: init,
             show: show,
             hide: hide,
             onSelectChange: onSelectChange,
-            signup: signup
+            signup: signup,
+            addAvatar: addAvatar
         };
 
     }());
